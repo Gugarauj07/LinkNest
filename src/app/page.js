8e38d6a3d620c0c6754109fc55c3e72791f59cbe
Gustavo De Araujo Lima <gugarauj07@MacBook-Air-de-Gustavo.local>
2023-12-17 22:13:56 -0400

    header

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,19 @@
-import Link from "next/link";
+import Header from "./components/header";
 
 export default function Home() {
   return (
     <main>
-      <header className="bg-white border-b flex justify-beetween">
-        <Link href="/">LinkNest</Link>
-        <nav>
-          <Link href="/about">About</Link>
-          <Link href="/pricing">Pricing</Link>
-          <Link href="/contact">Contact</Link>
-        </nav>
+      <Header />
 
-        <div>
-          <Link href="/login">Sign in</Link>
-          <Link href="/register">Create Account</Link>
-        </div>
-      </header>
-
-      <section>
+      <section pt-32>
+      <div className="max-w-md mb-8">
+        <h1 className="text-6xl font-bold">
+          Your single hub for all connections
+        </h1>
+        <h2 className="text-gray-500 text-xl mt-6">
+          Consolidate your online presence, social profiles, contact details and more in one place
+        </h2>
+      </div>
       </section>
     </main>
   )
