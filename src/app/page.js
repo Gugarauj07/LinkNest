51a9f14160f2cd087d7357aad77bce7c1053d484
gugarauj07 <gugaraujo01@gmail.com>
2023-12-17 22:25:51 -0400

    Add HeroForm component and update section styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import HeroForm from "./components/HeroForm";
 import Header from "./components/header";
 
 export default function Home() {
@@ -5,16 +6,18 @@ export default function Home() {
     <main>
       <Header />
 
-      <section pt-32>
-      <div className="max-w-md mb-8">
-        <h1 className="text-6xl font-bold">
-          Your single hub for all connections
-        </h1>
-        <h2 className="text-gray-500 text-xl mt-6">
-          Consolidate your online presence, social profiles, contact details and more in one place
-        </h2>
-      </div>
+      <section className="p-4">
+        <div className="max-w-md mb-8">
+          <h1 className="text-6xl font-bold">
+            Your single nest for all connections
+          </h1>
+          <h2 className="text-gray-500 text-xl mt-6">
+            Consolidate your online presence, social profiles, contact details and more in one place
+          </h2>
+        </div>
+        <HeroForm />
       </section>
+      
     </main>
   )
 }
