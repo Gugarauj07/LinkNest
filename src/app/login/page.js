d5bfd2aa39c7b0e81fe3a45419dfce1ac468d6fe
Gugarauj07 <gugaraujo01@gmail.com>
2023-12-19 17:06:50 -0300

    LoginPage

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,17 +1,17 @@
-import Header from "../components/Header";
+import LoginWithGoogle from "../components/LoginWithGoogle";
 
-export default function RootLayout({ children }) {
+export default function LoginPage() {
     return (
-      <html lang="en">
-        <body className={inter.className}>
-            <main>
-                <Header />
-                <div className="max-w-4xl mx-auto p-6">
-                    {children}
-                </div>
-            </main>
-        </body>
-      </html>
-    )
+      <div>
+        <div className="p-4 max-w-xs mx-auto">
+          <h1 className="text-4xl font-bold text-center mb-2">
+            Sign In
+          </h1>
+          <p className="text-center mb-6 text-gray-500">
+            Sign in to your account using one of the methods below
+          </p>
+          <LoginWithGoogle />
+        </div>
+      </div>
+    );
   }
-  
